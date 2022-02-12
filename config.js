3193e52d83a65aec0a645f820cbf426e5da853b0
vangdzhihi <99556983+vangdzhihi@users.noreply.github.com>
2022-02-13 02:14:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "xin chàooooo",
+    titleWeb:",
     introTitle: 'này người tôi yêu ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'eo ôiiii',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    Đằng sau cữêu đây là thương`,
+    btnIntro: 'eo ô em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Nào mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/vangdeeptryy.02
+    btnNo: 'Không nha
+    question: 'Trên hế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn 3', yêu thì yêu <33333333',
+    mess: 'Mình bihiều nhiều 😘😘',
+    messDesc: 'Nào mình chơi nha.',
+    btnAccept: 'Okiiiii
+    messLink: 'https://02
 }
